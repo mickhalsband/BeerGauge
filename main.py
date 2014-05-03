eabe4d0bdebe5b8862cf3a5d0317c52cd104c253
Mick Halsband <mick.halsband@gmail.com>
2014-05-03 16:27:32 +0300

    Buttons affect the progress bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.uix.button import Button
+from kivy.uix.progressbar import ProgressBar
 from kivy.uix.stacklayout import StackLayout
 
 __author__ = 'mick'
@@ -6,18 +7,31 @@ __author__ = 'mick'
 from kivy.app import App
 from kivy.uix.widget import Widget
 
-
 class BeerGauge(Widget):
     pass
-
 
 class ToolBar(StackLayout):
     pass
 
 
+class ProgressionTable:
+    table = {
+        'Beer': 20,
+        'Wine': 40,
+        'Whisky': 60,
+        'Other': 10
+    }
+
+
 class BGButton(Button):
-    def do_action(self, ªtext):
-        print text
+    def do_action(self):
+        print self.text
+        self.parent.progressBar.progress(ProgressionTable.table[self.text])
+
+
+class BeerGaugeBar(ProgressBar):
+    def progress(self, change):
+        self.value += change
 
 
 class BeerGaugeApp(App):
