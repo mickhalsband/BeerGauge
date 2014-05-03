648188de56b375ea06374e7ad3d47934d1b48a8a
Mick Halsband <mick.halsband@gmail.com>
2014-05-03 16:42:11 +0300

    Automatically decrease levels according to configured interval and step size

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.stacklayout import StackLayout
@@ -7,8 +8,10 @@ __author__ = 'mick'
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+
 class BeerGauge(Widget):
     pass
+
 
 class ToolBar(StackLayout):
     pass
@@ -30,8 +33,21 @@ class BGButton(Button):
 
 
 class BeerGaugeBar(ProgressBar):
+    """
+    Automatically decrease levels according to pre-configured interval and step
+    """
+    decrease_step = 5
+    decrease_interval = 0.5
+
+    def __init__(self, **kwargs):
+        super(BeerGaugeBar, self).__init__(**kwargs)
+        Clock.schedule_interval(self.decrease_levels, self.decrease_interval)
+
     def progress(self, change):
         self.value += change
+
+    def decrease_levels(self, *largs):
+        self.value -= self.decrease_step
 
 
 class BeerGaugeApp(App):
